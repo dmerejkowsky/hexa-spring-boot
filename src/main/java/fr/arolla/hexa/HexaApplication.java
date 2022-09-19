b195bb22f91b2205b77639208d1751680ae56763
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-09-19 11:06:16 +0200

    initial commit

---
+++ src/main/java/fr/arolla/hexa/HexaApplication.java
@@ -0,0 +1,13 @@
+package fr.arolla.hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaApplication.class, args);
+	}
+
+}
